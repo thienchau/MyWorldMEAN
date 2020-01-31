6cccebfc892040a6511bf304f1304cbdabcc85ce
Thien Chau <duythien0110@gmail.com>
2020-01-31 16:49:05 -0600

    user login, logout

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const userController = require('../controllers/user');
 
 const User = require('../models/user');
 
 const router = express.Router();
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
   const body = req.body;
-  console.log(body);
-
-  const result = userController.register(body)
+  const result = await userController.register(body)
   if (result.success) {
     res.json(result);
   } else {
@@ -19,37 +15,14 @@ router.post("/register", (req, res, next) => {
   }
 });
 
-router.post('/login', (req, res, next) => {
-  let fetchedUser;
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          message: 'Auth failed'
-        })
-      }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      console.log(result);
-      if (!result) {
-        return res.status(401).json({
-          message: 'Auth failed'
-        })
-      }
-      const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id },
-        process.env.SECRETE_KEY,
-        { expiresIn: '1h' });
-      res.status(200).json({
-        token: token
-      })
-    })
-    .catch(err => {
-      return res.status(401).json({
-        message: 'Auth failed'
-      })
-    })
+router.post('/login', async (req, res, next) => {
+  const body = req.body;
+  const result = await userController.login(body)
+  if (result.success) {
+    res.json(result);
+  } else {
+    next(result);
+  }
 });
 
 router.post('/follow/:id', async function (req, res, next) {
