dc7839ee06191c5ceecc842c0efa2c9346d523fd
XuanDatPhan <phanxuandat1995@gmail.com>
2020-01-31 14:39:15 -0600

    Fix API follow

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,4 +1,5 @@
 const {errors, jsonError, jsonSuccess} = require("../utils/system");
+const mongoose = require('mongoose');
 const Follow = require('../models/follow');
 
 const example = async () => {
@@ -7,13 +8,17 @@ const example = async () => {
 
 const followUser = async (follower, following) => {
     try {
-        const checkExist = Follow.findOne({ follower, following });
+        const fake1 = mongoose.Types.ObjectId();
+        const fake2 = mongoose.Types.ObjectId();
+        console.log(fake1);
+        console.log(fake2);
+        const checkExist = await Follow.findOne({ follower: fake1, following: fake2 });
         if (!checkExist) {
-            let newFollow = new Follow({
-                follower,
-                following
-            });
-            await newFollow.save();
+            let newFollow = await new Follow({
+                follower: fake1,
+                following: fake2
+            }).save();
+            console.log(newFollow);
         }
         return jsonSuccess();
     } catch (e) {
--- server/models/follow.js
+++ server/models/follow.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = require("mongoose");
 
 const followSchema = mongoose.Schema({
-    follower: { type: Schema.Types.ObjectId, ref: 'User' },
-    following: { type: Schema.Types.ObjectId, ref: 'User' },
+    follower: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
+    following: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
 });
 
-module.exports = mongoose.model('Follow', followSchema);
+module.exports = mongoose.model('Follows', followSchema);
