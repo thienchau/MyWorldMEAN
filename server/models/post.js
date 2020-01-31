7aa36d225df304c09618c7a05d213cfaf69e6e74
Sonnguyenh <Sonnguyenh>
2020-01-31 14:49:53 -0600

    update post

--- server/models/post.js
+++ server/models/post.js
@@ -1,10 +1,31 @@
-const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema({
-    id :{type: ObjectId},
-    create_date: { type: Date},
-    content: { type: String, require: true },
-    user: { type: String}
+const mongoose = require('mongoose');
+const Schema = require('mongoose/schema');
+const postSchema = Schema({
+    id: {type: Schema.ObjectId},
+    create_date: {type: Date},
+    content: {type: String, require: true},
+    user: {
+        type: Schema.ObjectId,
+        ref: 'Users'
+    },
+    media: {
+        url: String,
+        type: String,
+    },
+    comment: [{
+        content: String,
+        user: {
+            type: Schema.ObjectId,
+            ref: 'Users'
+        }
+    }],
+    like: [{
+        user: {
+            type: Schema.ObjectId,
+            ref: 'Users'
+        }
+    }]
 });
 
 module.exports = mongoose.model('Posts', postSchema);
