6d7e7ab388fdba83a200b190802d6f7ec0207de0
Thien Chau <duythien0110@gmail.com>
2020-01-31 21:11:34 -0600

    add auth services

---
+++ client/src/app/core/model/index.js
@@ -0,0 +1,9 @@
+export * from './user.model';
+export * from './token.model';
+export * from './follow.model';
+export * from './postNotification.model';
+export * from './adv.model';
+export * from './comment.model';
+export * from './media.model';
+
+
---
+++ client/src/app/core/service/index.js
@@ -0,0 +1,6 @@
+export * from './auth-guard.service';
+export * from './jwt.service';
+export * from './api.service';
+export * from './auth.service';
+export * from './ads.service';
+export * from './auth-guard.service';
