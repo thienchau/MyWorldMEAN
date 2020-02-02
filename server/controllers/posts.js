3c989c0cb8c3ba61ad4246f37d1745f91aa85042
Sonnguyenh <Sonnguyenh>
2020-02-02 16:51:03 -0600

    count post and search

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -147,7 +147,12 @@ const getPostByUserId = async (userId, page) => {
 
 const search = async function (req) {
     try {
-        let posts = await Post.find({"content": {"$regex": req.params.key, "$options": "i"}}).populate('user').limit(10);
+        let posts = await Post.find({
+            "content": {
+                "$regex": req.params.key,
+                "$options": "i"
+            }
+        }).populate('user').limit(10).sort({createdDate: -1});
         setupLikePosts(posts, req.user._id);
         return jsonSuccess(posts);
     } catch (e) {
@@ -162,14 +167,13 @@ const setupLikePosts = function (posts, userId) {
 const setupLikePost = function (post, userId) {
     if (post) {
         post.liked = false;
-        let num = 0;
         if (post.likes) {
-            num = post.likes.length;
             post.likes.findIndex((f) => {
                 post.liked = f.equals(userId)
             });
         }
-        post.likeNum = num;
+        post.likeNum = post.likes ? post.likes.length : 0;
+        post.commentNum = post.comments ? post.comments.length : 0;
     }
 };
 module.exports = {create, findById, getNewFeed, likePost, comment, search, getPostByUserId};
