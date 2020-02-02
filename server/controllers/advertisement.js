b07b1b1d7a7a70b08847e068c7cb99934fcd3076
Ha Phuc <sfg7896@gmail.com>
2020-02-02 13:55:22 -0600

    refactor advertisement API

--- server/controllers/advertisement.js
+++ server/controllers/advertisement.js
@@ -2,21 +2,20 @@ const Advertisement = require('../models/advertisement');
 const User = require('../models/user')
 
 const getAdvertisement = async (req, res) => {
-    // const idUser;
-    // const user = await User.findById(id)
+    const user = req.user;
     const advertisements = await Advertisement
     .find({$and: [
         {$or: [
-            {'target.gender': 'male'}, //user.gender
+            {'target.gender': user.gender}, //user.gender
             {'target.gender': 'all'}
         ]},
         {$and: [
-            {'target.ageFrom': {$lte: 20}}, //user.age
-            {'target.ageTo': {$gte: 20}} //user.age
+            {'target.ageFrom': {$lte: user.age}}, //user.age
+            {'target.ageTo': {$gte: user.age}} //user.age
         ]},
         {$or: [
             {'target.zipCode': 'all'},
-            {'target.zipCode': '52556'} //user.address.zip_code
+            {'target.zipCode': user.address.zip_code} //user.address.zip_code
         ]}
     ]}).limit(3)
     res.status(200).json(advertisements)
--- server/routes/advertisement.js
+++ server/routes/advertisement.js
@@ -6,15 +6,15 @@ const Advertisement = require('../models/advertisement');
 advertisementRouter.get('/', advertisementController.getAdvertisement)
 advertisementRouter.post('/', (req ,res) => {
     new Advertisement({
-        additional_content: 'Modern Web Application Course.',
-        content: 'This is a course.',
-        image_url: 'MWA',
-        url: 'MWA',
+        additional_content: 'friendmatch.com',
+        content: 'Make friends not ads.',
+        imageUrl: 'http://localhost:3000/ads/girl1.png',
+        url: 'https://friendmatch.com',
         target: {
-            age_from: 18,
-            age_to: 50,
+            ageFrom: 18,
+            ageTo: 50,
             gender: 'all',
-            zip_code: '52556'
+            zipCode: '52556'
         }
     
     }).save()
